20e74e13ddf9a77659a7ee064543a19430da48d4
Irina-Voronina <102693628+Irina-Voronina@users.noreply.github.com>
2023-05-17 11:08:25 +0300

    Добавьте файлы проекта.

---
+++ MVVMApp/Models/Client.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MVVMApp.Models
+{
+    public class Client
+    {
+        public string Transaction { get; set; }
+        public string Adress { get; set; }
+        public string Price { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ MVVMApp/Views/ClientPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using MVVMApp.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace MVVMApp.Views
+{
+    public partial class ClientPage : ContentPage
+    {
+        public ClientViewModel ViewModel { get; private set; }
+        public ClientPage(ClientViewModel vm)
+        {
+            InitializeComponent();
+            ViewModel = vm;
+            BindingContext = ViewModel;
+        }
+    }
+}
